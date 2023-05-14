4b31f8c31ef12cd22065ee26927759aa1b0fcb3b
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-05-14 21:10:10 +0300

    RSA cipher implementation added

--- rocket-web/src/lib.rs
+++ rocket-web/src/lib.rs
@@ -1,1 +1,1 @@
-pub mod encryptor;
+pub mod cipher;
--- rocket-web/src/main.rs
+++ rocket-web/src/main.rs
@@ -1,4 +1,4 @@
-use our_package::encryptor::{rot13, Encryptable};
+use our_package::cipher::{rot13, rsa, Cipher};
 use std::io;
 
 fn main() {
@@ -12,6 +12,22 @@ fn main() {
 
     println!(
         "Your encrypted string: {}",
-        rot13::Rot13(user_input).encrypt()
+        rot13::Rot13(user_input).encrypted_string().unwrap()
     );
+
+    println!("Input the string you want to encrypt:");
+
+    let mut user_input = String::new();
+
+    io::stdin()
+        .read_line(&mut user_input)
+        .expect("Cannot read input");
+
+    let encrypted_input = rsa::Rsa::new(user_input).expect("");
+    let encrypted_string = encrypted_input.encrypted_string().expect("");
+
+    println!("Your encrypted string: {}", encrypted_string);
+
+    let decrypted_string = encrypted_input.original_string().expect("");
+    println!("Your original string: {}", decrypted_string);
 }
