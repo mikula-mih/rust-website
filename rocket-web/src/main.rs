e0215f9abe524da890482c5b783e7cfc695afb1a
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-05-14 21:20:29 +0300

    refactored encryption project and started initial Rocket project

--- rocket-web/src/main.rs
+++ rocket-web/src/main.rs
@@ -1,33 +1,14 @@
-use our_package::cipher::{rot13, rsa, Cipher};
-use std::io;
+#[macro_use]
+extern crate rocket;
 
-fn main() {
-    println!("Input the string you want to encrypt:");
+use rocket::{Build, Rocket};
 
-    let mut user_input = String::new();
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, Rocket!"
+}
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Cannot read input");
-
-    println!(
-        "Your encrypted string: {}",
-        rot13::Rot13(user_input).encrypted_string().unwrap()
-    );
-
-    println!("Input the string you want to encrypt:");
-
-    let mut user_input = String::new();
-
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Cannot read input");
-
-    let encrypted_input = rsa::Rsa::new(user_input).expect("");
-    let encrypted_string = encrypted_input.encrypted_string().expect("");
-
-    println!("Your encrypted string: {}", encrypted_string);
-
-    let decrypted_string = encrypted_input.original_string().expect("");
-    println!("Your original string: {}", decrypted_string);
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![index])
 }
