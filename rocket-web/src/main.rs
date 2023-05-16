3e08db4945ad27033440a236b9e45d4cf6ba946e
Mikula Mikhail <mikula.mikhail@gmail.com>
2023-05-16 14:20:25 +0300

    intorduced persistent storage for Rocket web and some refactoring

--- rocket-web/src/main.rs
+++ rocket-web/src/main.rs
@@ -1,16 +1,22 @@
 #[macro_use]
 extern crate rocket;
 
-use lazy_static::lazy_static;
-use rocket::http::{Status, ContentType};
+use rocket::fairing::{self, Fairing, Info, Kind};
+use rocket::fs::{relative, NamedFile};
+use rocket::http::{ContentType, Header, Status};
 use rocket::request::{FromParam, Request};
 use rocket::response::{self, Responder, Response};
-use rocket::{Build, Rocket};
-use rocket::fs::{NamedFile, relative};
-use std::collections::HashMap;
+use rocket::{Build, Data, Orbit, Rocket};
+use rocket_db_pools::{
+    sqlx,
+    sqlx::{FromRow, PgPool},
+    Connection, Database,
+};
 use std::io::Cursor;
-use std::vec::Vec;
 use std::path::Path;
+use std::sync::atomic::{AtomicU64, Ordering};
+use std::vec::Vec;
+use uuid::Uuid;
 
 #[derive(FromForm)]
 struct Filters {
@@ -50,16 +56,16 @@ fn default_response<'r>() -> response::Response<'r> {
         .finalize()
 }
 
-#[derive(Debug)]
+#[derive(Debug, FromRow)]
 struct User {
-    uuid: String,
+    uuid: Uuid,
     name: String,
-    age: u8,
-    grade: u8,
+    age: i16,
+    grade: i16,
     active: bool,
 }
 
-impl<'r> Responder<'r, 'r> for &'r User {
+impl<'r> Responder<'r, 'r> for User {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'r> {
         let base_response = default_response();
         let user = format!("Found user: {:?}", self);
@@ -71,9 +77,9 @@ impl<'r> Responder<'r, 'r> for &'r User {
     }
 }
 
-struct NewUser<'a>(Vec<&'a User>);
+struct NewUser(Vec<User>);
 
-impl<'r> Responder<'r, 'r> for NewUser<'r> {
+impl<'r> Responder<'r, 'r> for NewUser {
     fn respond_to(self, _: &'r Request<'_>) -> response::Result<'r> {
         let base_response = default_response();
         let user = self
@@ -90,43 +96,100 @@ impl<'r> Responder<'r, 'r> for NewUser<'r> {
     }
 }
 
-lazy_static! {
-    static ref USERS: HashMap<&'static str, User> = {
-        let mut map = HashMap::new();
-        map.insert(
-            "3e3dd4ae-3c37-40c6-aa64-7061f284ce28",
-            User {
-                uuid: String::from("3e3dd4ae-3c37-40c6-aa64-7061f284ce28"),
-                name: String::from("John Doe"),
-                age: 18,
-                grade: 1,
-                active: true,
-            },
-        );
-        map
-    };
+struct VisitorCounter {
+    visitor: AtomicU64,
 }
 
+impl VisitorCounter {
+    fn increment_counter(&self) {
+        self.visitor.fetch_add(1, Ordering::Relaxed);
+        println!(
+            "The number of visitor is: {}",
+            self.visitor.load(Ordering::Relaxed)
+        );
+    }
+}
+
+#[rocket::async_trait]
+impl Fairing for VisitorCounter {
+    fn info(&self) -> Info {
+        Info {
+            name: "Visitor Counter",
+            kind: Kind::Ignite | Kind::Liftoff | Kind::Request,
+        }
+    }
+
+    async fn on_ignite(&self, rocket: Rocket<Build>) -> fairing::Result {
+        println!("Setting up visitor counter");
+        Ok(rocket)
+    }
+
+    async fn on_liftoff(&self, _: &Rocket<Orbit>) {
+        println!("Finish setting up visitor counter");
+    }
+
+    async fn on_request(&self, _: &mut Request<'_>, _: &mut Data<'_>) {
+        self.increment_counter();
+    }
+}
+
+const X_TRACE_ID: &str = "X-TRACE-ID";
+struct XTraceId {}
+
+#[rocket::async_trait]
+impl Fairing for XTraceId {
+    fn info(&self) -> Info {
+        Info {
+            name: "X-TRACE-ID Injector",
+            kind: Kind::Request | Kind::Response,
+        }
+    }
+
+    async fn on_request(&self, req: &mut Request<'_>, _: &mut Data<'_>) {
+        let header = Header::new(X_TRACE_ID, Uuid::new_v4().to_hyphenated().to_string());
+        req.add_header(header);
+    }
+
+    async fn on_response<'r>(&self, req: &'r Request<'_>, res: &mut Response<'r>) {
+        let header = req.headers().get_one(X_TRACE_ID).unwrap();
+        res.set_header(Header::new(X_TRACE_ID, header));
+    }
+}
+
+#[derive(Database)]
+#[database("main_connection")]
+struct DBConnection(PgPool);
+
 #[get("/user/<uuid>", rank = 1, format = "text/plain")]
-fn user(uuid: &str) -> Option<&User> {
-    USERS.get(uuid)
+async fn user(mut db: Connection<DBConnection>, uuid: &str) -> Result<User, Status> {
+    let parsed_uuid = Uuid::parse_str(uuid).map_err(|_| Status::BadRequest)?;
+
+    let user = sqlx::query_as!(User, "SELECT * FROM users WHERE uuid = $1", parsed_uuid)
+        .fetch_one(&mut *db)
+        .await;
+    user.map_err(|_| Status::NotFound)
 }
 
 #[get("/users/<name_grade>?<filters..>")]
-fn users(name_grade: NameGrade, filters: Option<Filters>) -> Result<NewUser, Status> {
-    let users: Vec<&User> = USERS
-        .values()
-        .filter(|user| user.name.contains(&name_grade.name) && user.grade == name_grade.grade)
-        .filter(|user| {
-            if let Some(fts) = &filters {
-                user.age == fts.age && user.active == fts.active
-            } else {
-                true
-            }
-        })
-        .collect();
+async fn users(
+    mut db: Connection<DBConnection>,
+    name_grade: NameGrade<'_>,
+    filters: Option<Filters>,
+) -> Result<NewUser, Status> {
+    let mut query_str = String::from("SELECT * FROM users WHERE name LIKE $1 AND grade = $2");
+    if filters.is_some() {
+        query_str.push_str(" AND age = $3 AND active = $4");
+    }
+    let mut query = sqlx::query_as::<_, User>(&query_str)
+        .bind(format!("%{}%", &name_grade.name))
+        .bind(name_grade.grade as i16);
+    if let Some(fts) = &filters {
+        query = query.bind(fts.age as i16).bind(fts.active);
+    }
+    let unwrapped_users = query.fetch_all(&mut *db).await;
+    let users: Vec<User> = unwrapped_users.map_err(|_| Status::InternalServerError)?;
     if users.is_empty() {
-        Err(Status::Forbidden)
+        Err(Status::NotFound)
     } else {
         Ok(NewUser(users))
     }
@@ -134,7 +197,9 @@ fn users(name_grade: NameGrade, filters: Option<Filters>) -> Result<NewUser, Sta
 
 #[get("/favicon.png")]
 async fn favicon() -> NamedFile {
-    NamedFile::open(Path::new(relative!("static")).join("favicon.png")).await.unwrap()
+    NamedFile::open(Path::new(relative!("static")).join("favicon.png"))
+        .await
+        .unwrap()
 }
 
 #[catch(404)]
@@ -148,6 +213,17 @@ fn forbidden(req: &Request) -> String {
 }
 
 #[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount("/", routes![user, users, favicon]).register("/", catchers![not_found, forbidden])
+async fn rocket() -> Rocket<Build> {
+    let visitor_counter = VisitorCounter {
+        visitor: AtomicU64::new(0),
+    };
+
+    let x_trace_id = XTraceId {};
+
+    rocket::build()
+        .attach(DBConnection::init())
+        .attach(visitor_counter)
+        .attach(x_trace_id)
+        .mount("/", routes![user, users, favicon])
+        .register("/", catchers![not_found, forbidden])
 }
